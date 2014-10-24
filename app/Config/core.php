1f52ea6ecd424e0b4a078adf025948e8db3c7882
Mateusz Grosiak <mateuszgrosiak@gmail.com>
2014-10-24 21:23:33 +0200

    security salt, cipherSeed

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'ASDgfdfsGS4s4tGFSEB3wrvgfXFSW#bSDGF');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '798643241276973547854236980978');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
